8f0507d4253fa256a4fee976cbf66b31cece5942
竹内快 <kaitakeuchi@Kai-MacBook-Pro.local>
2025-03-10 15:01:00 +0900

    一時的にESLintのエラーを警告に変更

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn", // エラーから警告に変更
+      "@typescript-eslint/no-explicit-any": "warn" // エラーから警告に変更
+    }
+  }
 ];
 
 export default eslintConfig;
