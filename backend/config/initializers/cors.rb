ff3d7456f50565b6041c5ae999eadc690be68b4e
竹内快 <kaitakeuchi@Kai-MacBook-Pro.local>
2025-03-18 01:37:18 +0900

    cors.rbの編集

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://conductor-rho.vercel.app', 'http://localhost:3000'
+    origins '*'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Authorization'], # Authorization ヘッダーを公開（認証トークンのため
+      expose: ['Authorization']
   end
 end
